8d5effda860a46ebb371375c9868adc2872ea68d
Dalouma <73500071+Dalouma@users.noreply.github.com>
2023-05-14 16:25:53 -0700

    turned off debug mode

--- src/main.js
+++ src/main.js
@@ -51,7 +51,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
+            // debug: true,
             gravity: {
                 x: 0,
                 y: 0
